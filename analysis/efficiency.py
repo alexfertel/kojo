142a3f0a1be894e4c9463b574dbb75d677ffed7b
Alexander Gonzalez <alexfertel97@gmail.com>
2019-03-22 15:07:59 -0400

    Minor fixes

--- analysis/efficiency.py
+++ analysis/efficiency.py
@@ -4,7 +4,7 @@ from config import LAMBDA
 
 
 def analyze(runs):
-    print(f"Lambda is {LAMBDA}")
+    print(f"1 / Lambda is {LAMBDA / 60} minutes")
     for i in range(runs):
         # Compute next log file
         index = int(get_env("RUNS")) + 1
@@ -19,7 +19,7 @@ def analyze(runs):
         # Process results from kdown
         more_than_five = [1 if t >= 300 else 0 for t in kdown.deltas]
         pdown = (sum(more_than_five) * 100) / len(more_than_five)
-        print(f"Simulation {i}\tpercent of clients who waited more than 5 minutes in type `down` is\t{pdown}")
+        print(f"Simulation {i}:\tCustomers who waited > 5 minutes in a normal   kitchen\t% {pdown}")
 
         # With three employees
         kup = Kitchen(upgraded=1)  # `EMPLOYEES_COUNT + 1` employees.
@@ -28,7 +28,7 @@ def analyze(runs):
         # Process results from kup
         more_than_five = [1 if t >= 300 else 0 for t in kup.deltas]
         pup = (sum(more_than_five) * 100) / len(more_than_five)
-        print(f"Simulation {i}\tpercent of clients who waited more than 5 minutes in type `up` is\t{pup}")
+        print(f"Simulation {i}:\tCustomers who waited > 5 minutes in a upgraded kitchen\t% {pup}")
 
         # Update `RUNS` env var
         set_env("RUNS", index)
